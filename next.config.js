400ac8c4e3146e2899da367cff70810a50bac8ca
Koda <koda@horizons.gg>
2024-11-12 16:39:12 +1000

    Fixed Redirects

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'asotmilsim.net',
+          },
+        ],
+        destination: 'https://www.asotmilsim.net/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-})
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = withBundleAnalyzer(nextConfig);
