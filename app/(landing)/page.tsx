0a953b68bfebe14218ebf2800e0583973610ae86
Koda <koda@horizons.gg>
2024-11-18 15:51:20 +1000

    Thomo Mode

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -110,10 +110,30 @@ export default function Page() {
 
 
 			{thomo ? (
-				<Container title='Enter Major General Thomas' background={ThomoFire} sx={{ maxWidth: 'max-w-md', bannerHeight: 'lg' }}>
+				<Container title='Enter Major General Thomas' background={ThomoFire} sx={{ maxWidth: 'max-w-xl', bannerHeight: 'lg' }}>
 					
-					<div>
-
+					<div className='grid grid-cols-2 md:grid-cols-4 gap-5'>
+						{[
+							'meme.gif',
+							'gifPerms.gif',
+							'mci.gif',
+							'mci2.gif',
+							'meme_1.gif',
+							'meme_2.gif',
+							'meme_3.gif',
+							'meme_4.gif',
+							'meme_5.gif',
+							'meme_6.gif',
+							'meme_7.gif',
+							'meme_8.gif',
+							'meme_9.gif',
+							'meme_10.gif',
+							'meme.gif',
+							'simpsons.gif',
+							'image.png',
+						].map((image, i) => (
+							<img key={i} src={`/thomo/${image}`} className='m-auto' />
+						))}
 					</div>
 
 				</Container>
