8b63518a9ff3f5b842b7e4555fa2d1303956e599
Koda <koda@horizons.gg>
2024-11-21 22:51:56 +1000

    Update list.tsx

--- components/member/list.tsx
+++ components/member/list.tsx
@@ -14,9 +14,6 @@ export default function Members() {
     const [members, setMembers] = useState<User[]>([])
 
 
-    useEffect(() => {
-        fetchMembers()
-    }, [])
 
     useEffect(() => {
         fetchMembers(search)
@@ -42,7 +39,7 @@ export default function Members() {
             <div className='flex flex-col flex-grow overflow-hidden'>
                 <div className='relative flex-grow overflow-auto'>
                     <div className='absolute w-full flex flex-col gap-3 pr-5'>
-                        {members.map((member, i) => <Member key={member._id} member={member.discord} />)}
+                        {/* {members.map((member, i) => <Member key={member._id} member={member.discord} />)} */}
                     </div>
                 </div>
             </div>
