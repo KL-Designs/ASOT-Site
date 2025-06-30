629fcc1db1f17b1493f0c4ae3a95ddfff68c26ed
Koda <koda@horizons.gg>
2025-06-30 16:49:54 +1000

    Updated Sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.asotmilsim.net',
+      url: 'https://www.asotmilsim.com',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://www.asotmilsim.net/about',
+      url: 'https://www.asotmilsim.com/about',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: 'https://www.asotmilsim.net/orbat',
+      url: 'https://www.asotmilsim.com/orbat',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.6,
     },
     {
-      url: 'https://www.asotmilsim.net/partnerships',
+      url: 'https://www.asotmilsim.com/partnerships',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
-      url: 'https://www.asotmilsim.net/support',
+      url: 'https://www.asotmilsim.com/support',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
