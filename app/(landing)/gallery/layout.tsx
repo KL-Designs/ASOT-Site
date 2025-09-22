ff800c646d864662b81c7861978acbba08a88929
Koda <koda@horizons.gg>
2025-09-22 20:25:39 +1000

    Fixed Meta data

--- app/(landing)/gallery/layout.tsx
+++ app/(landing)/gallery/layout.tsx
@@ -1,27 +1,6 @@
-import { Metadata, Viewport } from "next"
-import Image from 'next/image'
-
-import { Typography } from '@mui/material'
 import Container from '@/components/container'
 
 import Banner from '@/public/images/home/snowwalk1.png'
-
-
-export const viewport: Viewport = {
-	themeColor: "#9d000c"
-}
-
-export const metadata: Metadata = {
-	title: "Gallery | Australian Special Operations Taskforce",
-	description: "Australia's premiere ARMA 3 mil sim community. Recruiting now! 17+ unless vouched for by a current member. Any experience level welcome and no DLC is required.",
-	keywords: ["arma", "arma 3", "australian", "special", "operations", "taskforce", "asot", "milsim"],
-	twitter: {
-		images: `${process.env.NEXT_PUBLIC_BASEURL}/logo.png`
-	},
-	openGraph: {
-		images: `${process.env.NEXT_PUBLIC_BASEURL}/logo.png`
-	}
-}
 
 
 
