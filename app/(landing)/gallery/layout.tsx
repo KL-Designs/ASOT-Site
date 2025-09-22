748184a049be10dcf2eb544d493d2ee512c3c154
Koda <koda@horizons.gg>
2025-09-22 19:23:11 +1000

    Fixed Layout issues

--- app/(landing)/gallery/layout.tsx
+++ app/(landing)/gallery/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata, Viewport } from "next"
 import Image from 'next/image'
 
 import { Typography } from '@mui/material'
+import Container from '@/components/container'
 
 import Banner from '@/public/images/home/snowwalk1.png'
 
@@ -26,34 +27,12 @@ export const metadata: Metadata = {
 
 export default function Page({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<div className='h-full w-full'>
+		<Container title='GALLERY' background={Banner} sx={{ bannerHeight: 'sm', maxWidth: '100%', padding: '0px' }}>
+			<div className='py-5 m-auto px-10'>
 
-			<div className='relative w-full h-[40vh] md:h-[60vh] flex flex-col justify-center items-center'>
-				<Image src={Banner} alt='Banner' fill className='object-cover object-center blur-[2px]' loading='eager' />
+				{children}
 
-				<div className='max-w-[1000px] mt-[70px] mx-10 flex flex-col justify-center gap-5'>
-					<div className='flex flex-col justify-center items-center gap-3' style={{
-						backgroundColor: 'rgba(0, 0, 0, 0.5)',
-						backdropFilter: 'blur(10px)',
-						border: '2px solid #db001d',
-						padding: '15px'
-					}}>
-						<Typography className='hidden md:flex text-[34px] md:text-[60px]' variant='h1' align='center' fontWeight={700} fontFamily={'inherit'} letterSpacing={4}>
-							GALLERY
-						</Typography>
-					</div>
-				</div>
 			</div>
-
-
-			<div className="w-full" style={{ borderTop: '1px solid #db001d' }}>
-				<div className='py-5 m-auto px-10'>
-
-					{children}
-
-				</div>
-			</div>
-
-		</div>
+		</Container>
 	)
 }
